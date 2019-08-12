89c92281728e921ba57879ef5b6de9b59bb51f0f
Joshua Shinkle <2020joshuashinkle@C02RN58ZGFWM.local>
2019-08-12 15:07:08 -0400

    First commit

---
+++ include/global.h
@@ -0,0 +1,8 @@
+#include "okapi/api.hpp"
+
+extern pros:: Controller weber;
+
+extern pros::Motor driveRightFront;
+extern pros::Motor driveRightBack;
+extern pros::Motor driveLeftFront;
+extern pros::Motor driveLeftBack;
